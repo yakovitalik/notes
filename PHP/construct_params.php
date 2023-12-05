74eea064207c3cdf96ff5db6a7db9639b2a89226
Vitaliy Yakovlev <yakovitalik@mail.ru>
2023-12-05 23:28:51 +0300

    PHP Folder, Docker edited

---
+++ PHP/construct_params.php
@@ -0,0 +1,22 @@
+﻿<?php
+class Point {
+	private $x;
+	private $y;
+
+	public function __construct(int $x, int $y) {
+			$this->x = $x;
+			$this->y = $y;
+	}
+
+	public function getX() : int {
+		return $this->x;
+	}
+
+	public function getY() : int {
+		return $this->y;
+	}
+
+	public function distance() : float {
+		return sqrt($this->x ** 2 + $this->y ** 2);
+	}
+}
---
+++ PHP/construct_params_use.php
@@ -0,0 +1,7 @@
+﻿<?php
+require_once 'construct_params.php';
+
+// $point = new Point();	// PHP Ftatal Error: Uncaught ArgumentError
+$point = new Point(x: 5, y: 3);
+
+echo $point->distance();	// 5.8309518948453
